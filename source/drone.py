f62d53ff3d1ca33b8ab70b201ebc07370321c091
amrish1222 <amrish1222@gmail.com>
2019-11-10 16:20:03 -0500

    updated stubs for position, charge, tour and state

--- source/drone.py
+++ source/drone.py
@@ -10,8 +10,8 @@ import numpy as np
 
 class Drone:
     def __init__(self):
-        self.curPos = [0,0]
-        self. curVel = [0,0]
+        self.curPos = np.array([0,0])
+        self. curVel = np.array([0,0])
         self.size = 0.4 # Diameter of the drone
         self.tourTaken = [] # list of positions that the drone has taken
         self.isDocked = False # whether the drone is docked to the mobile robot
@@ -22,22 +22,34 @@ class Drone:
         
     def setParams(self, vel, dock):
         self.curVel = vel
-        if np.linalg.norm(self.curPos-self.parentPos) < self.dockingThreshold:
+        distFromParent = np.linalg.norm(self.curPos - self.parentPos)
+        if distFromParent < self.dockingThreshold:
             self.isDocked = True
         else:
             print("Can not dock: out of dock range")
             
-    def updateState(self, parentPos):
-        pass
+    def updateState(self, parentPos, timeStep):
+        self.updatePos(parentPos,timeStep)
+        self.updateCharge(timeStep)
+        self.updateTour()
     
-    def updatePos(self):
-        pass
+    def updatePos(self, parentPos, timeStep):
+        if self.isDocked:
+            newPosition = parentPos
+        else:
+            newPosition = self.curPos + self.curVel * timeStep
+        self.curPos = newPosition
     
-    def updateCharge(self):
-        pass
+    
+    def updateCharge(self, timeStep):
+        self.currentCharge += timeStep
+        if self.currentCharge >= self.maxCharge:
+            self.isDocked = False
     
     def updateTour(self):
-        pass
+        if len(self.tourTaken) > 0:
+            if not self.tourTaken[-1] == self.curPos:
+              self.tourTaken.append(self.curPos)  
     
     def getState(self):
         time2dock = self.maxCharge - self.currentCharge
