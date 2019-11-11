3df76d6103a7b007a18a31f2240d8a5fe3cc4d3c
amrish1222 <amrish1222@gmail.com>
2019-11-11 01:01:12 -0500

    added stub for MobileRobot class

---
+++ source/mobile_robot.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# author Amrish Bakaran
+# author Adhesh
+# author Bala Murali
+# Copyright
+# brief Mobile robot class containing state, methods and history of the drone
+
+import numpy as np
+
+
+class MobileRobot:
+    def __init__(self):
+        self.curPos = np.array([0,0])
+        self. curVel = np.array([0,0])
+        self.size = 2 # mobile robot square side
+        self.tourTaken = [] # list of positions that the MR has taken
+        self.resourcePt = [] # detected resource collection pts
+        
+    def setParams(self, vel, dock):
+        self.curVel = vel
+    
+    def updatePos(self, parentPos, timeStep):
+        if self.isDocked:
+            newPosition = parentPos
+        else:
+            newPosition = self.curPos + self.curVel * timeStep
+        self.curPos = newPosition
+    
+    def updateTour(self):
+        if len(self.tourTaken) > 0:
+            if not self.tourTaken[-1] == self.curPos:
+              self.tourTaken.append(self.curPos)
+              
+    def getState(self):
+        return self.curPos, self.tourTaken
