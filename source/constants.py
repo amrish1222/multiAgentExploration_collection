58ee5876a09253d053020124afc2200392132c43
amrish1222 <amrish1222@gmail.com>
2019-11-20 16:44:09 -0500

    added constants file

---
+++ source/constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# author Amrish Bakaran
+# author Adheesh
+# author Bala Murali
+# Copyright
+# Constants
+
+screenHeight = 1000
+screenWidth = 1000
+arenaWidth = 20
+#numDrone = 2
+#numMobileRobots = 1 
