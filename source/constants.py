5b2d5a29a18f77a81608a9d02d726dac1d214e9d
Adheesh <adheeshchat@gmail.com>
2019-11-20 18:17:49 -0500

    constants updated

--- source/constants.py
+++ source/constants.py
@@ -8,5 +8,14 @@
 screenHeight = 1000
 screenWidth = 1000
 arenaWidth = 20
+arenaHeight = 20
+
 numDrone = 2
 numMobileRobots = 1
+
+BLACK=(0,0,0)
+AQUA=(0,255,255)
+
+timeStep=0.1
+maxDroneVelocity=2
+maxMobileRobVelocity=1
