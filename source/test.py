9284c0bad80f245bbcbd8f079ca7444440e072f5
bsaisudh <bsaisudh@terpmail.umd.edu>
2019-11-20 19:10:53 -0500

    corrected mobile random agent

--- source/test.py
+++ source/test.py
@@ -11,18 +11,21 @@ from env import Env
 import numpy as np
 import pygame
 import time 
+from mobileAgent import mobileRandomAgent
 
 env = Env(2, 1)
+mAgent = mobileRandomAgent()
+dAgent = [mobileRandomAgent(), mobileRandomAgent()]
 
 while True:
 	droneActions = []
 	docks = []
 	for i in range(2):
-		droneActions.append(np.random.randint(1,5))
+		droneActions.append(dAgent[i].getAction())
 		docks.append(False)
 	mrActions = []
 	for i in range(1):
-		mrActions.append(np.random.randint(1,5))
+		mrActions.append(mAgent.getAction())
 
 	print("droneActions:", droneActions)
 	print("mrActions:", mrActions)
