212e348419cb030336fddbf78621b5edd97f108b
Adheesh <adheeshc@github.com>
2019-12-07 18:20:11 -0500

    added path mobile agent

--- source/mobileAgent.py
+++ source/mobileAgent.py
@@ -7,39 +7,92 @@ Created on Wed Nov 20 17:16:45 2019
 """
 
 import numpy as np
+from constants import *
 np.set_printoptions(precision=3, suppress=True)
 
 class mobileRandomAgent:
-    def __init__(self):
+    def __init__(self,defined=False):
         self.prevAngle = 0
-        self.momentum = 0.7
+        self.momentum = 1
         self.currAngle = 0
         self.rand = 0
+        self.defined=defined
     
-    def getAction(self):
+    def getAction(self,mrPos):
         self.rand = np.random.uniform(-180,180)
-        newAngle = self.prevAngle + (1-self.momentum) * self.rand
+        currPos=mrPos
+        if self.defined:
+            newAngle = self.prevAngle + (1-self.momentum) * self.rand
+        else:
+            newAngle = self.prevAngle + (1-self.momentum)
         if newAngle < 0:
             self.currAngle = 360 + newAngle
         else:
             self.currAngle = newAngle
+        if currPos[0]==0:
+            self.currAngle=180
+        if currPos[1]==0:
+            self.currAngle=270
+        if currPos[0]==arenaWidth:
+            self.currAngle=0
+        if currPos[1]==arenaHeight:
+            self.currAngle=90
+
+            
         self.currAngle = self.currAngle % 360
         action = int(self.currAngle / 90)
+        print(action)
+        #print(action)
         self.prevAngle = self.currAngle
+
         return action + 1
+
+# class mobileAgent:
+#     def __init__(self):
+#         self.prevAngle = 0
+#         self.momentum = 1
+#         self.currAngle = 0
+#         self.rand = 0
+
+#     def getAction(self):
+#         newAngle = self.prevAngle + (1-self.momentum)
+#         if newAngle < 0:
+#             self.currAngle = 360 + newAngle
+#         else:
+#             self.currAngle = newAngle
+#         self.currAngle = self.currAngle % 360
+        
+#         action = int(self.currAngle / 90)
+#         self.prevAngle = self.currAngle
+#         return action + 1
+
+
 
 
 if __name__ == "__main__":
-    for mom in [0.1, 0.5, 0.9]:
-        act =[]
-        m = mobileRandomAgent()    
-        m.momentum = mom
-        for i in range(300):
-            act.append(m.getAction())
-        print(f"Momentum: {mom} --> \n \
-             Action 0: {act.count(0)}  \n\
-              Action 1: {act.count(1)} \n\
-              Action 2: {act.count(2)} \n\
-              Action 3: {act.count(3)} \n\
-              Action 4: {act.count(4)} \n")
+    
+    act =[]
+    m=mobileRandomAgent()
+    
+    for i in range(1000):
+        act.append(m.getAction(mrPos))
+
+
+    print(f"Momentum: {m.momentum} --> \n \
+        Action 0: {act.count(0)}  \n\
+        Action 1: {act.count(1)} \n\
+        Action 2: {act.count(2)} \n\
+        Action 3: {act.count(3)} \n\
+        Action 4: {act.count(4)} \n")
         
+        # m = mobileRandomAgent()    
+        # m.momentum = mom
+        # for i in range(300):
+        #     act.append(m.getAction())
+        # print(f"Momentum: {mom} --> \n \
+        #      Action 0: {act.count(0)}  \n\
+        #       Action 1: {act.count(1)} \n\
+        #       Action 2: {act.count(2)} \n\
+        #       Action 3: {act.count(3)} \n\
+        #       Action 4: {act.count(4)} \n")
+        # 
--- source/test.py
+++ source/test.py
@@ -13,14 +13,14 @@ import matplotlib.pyplot as plt
 from env import Env
 from drone import Drone
 from mobile_robot import MobileRobot
-from mobileAgent import mobileRandomAgent
+from mobileAgent import *
 from constants import *
 
 np.set_printoptions(precision=3, suppress=True)
 
 env = Env(1, 1)
 
-mAgent = mobileRandomAgent()
+mAgent = mobileRandomAgent(True)
 dAgent = [mobileRandomAgent(), mobileRandomAgent()]
 
 win_close_f = False
@@ -35,20 +35,21 @@ for episodes in range(NUM_EPISODES):
         droneActions = []
         docks = []
         for i in range(2):
-            droneActions.append(dAgent[i].getAction())
+            #droneActions.append(dAgent[i].getAction())
 #            droneActions.append(1)
             docks.append(False)
         mrActions = []
+        mrPos,_,_=env.mobilerobots[0].getState()
         for i in range(1):
-            mrActions.append(mAgent.getAction())
+            mrActions.append(mAgent.getAction(mrPos))
 #            mrActions.append(2)
-        
+
     #    dronePos, droneVel, droneCharge, dock, done= env.stepDrones(droneActions, docks)
     #    mrPos, mrVel,localArea = env.stepMobileRobs(mrActions)
         mrPos, mrVel, localArea, dronePos, droneVel, droneCharge, dock, reward, done = env.step(mrActions, droneActions, docks)
-        print(dronePos, mrPos, droneCharge)
+
+        #print(dronePos, mrPos, droneCharge)
         env.render()
-        
         # check if any unexploredArea in local Area for static MobileRobot test
         if not np.any(localArea[0] == 50):
             break
